4eeeaedac472ad75435d06628262d8c04b50639d
A-Littlejohn <72235917+A-Littlejohn@users.noreply.github.com>
2020-11-07 16:13:23 -0800

    Started Workout View Model- Azarius Littlejohn

---
+++ MySwoleMate.Models/WorkoutViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySwoleMate.Models
+{
+    class WorkoutViewModel
+    {
+        public int WorkoutID { get; set; }
+        public string Exercise1 { get; set; }
+        public int Exercise1Sets { get; set; }
+        public int Exercise1Reps { get; set; }
+        
+        public string DisplayExercise1
+        {
+            get
+            {
+               return this.Exercise1 + ": " + this.Exercise1Sets + "x" + this.Exercise1Reps;
+            }
+        }
+    }
+}
+
